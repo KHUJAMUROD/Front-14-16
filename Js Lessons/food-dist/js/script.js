5e95750178658b8346ae5d4ddded6f53e72c07e2
Hunter <xujanov.xujamurod@gmail.com>
2024-10-14 16:01:06 +0500

    lesson for 14-16 group

--- Js Lessons/food-dist/js/script.js
+++ Js Lessons/food-dist/js/script.js
@@ -1,5 +1,44 @@
 'use strict';
 
+
+window.addEventListener('DOMContentLoaded', () => {
+    consttabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('tabcontent'),
+        tabsParent = document.querySelector('.tabheader__item');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+        });
+        tab.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+
+
+
+    }
+
+
+    function showTabContent(i) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+
+    })
+
+});
+
+
+
+
+
+
+
 // window.addEventListener('DOMContentLoaded', () => {
 //     consttabs = document.querySelectorAll('.tabheader__item'),
 //         tabsContent = document.querySelectorAll('tabcontent'),
