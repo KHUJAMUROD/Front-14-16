6bdb61c627c2d6a0cfeabeb84026aad32974b794
Hunter <xujanov.xujamurod@gmail.com>
2024-10-23 15:52:27 +0500

    modified Weak-map Weak-Set

--- Js Lessons/WeakMap-WeakSet/script.js
+++ Js Lessons/WeakMap-WeakSet/script.js
@@ -1,57 +1,124 @@
+
+// Baxa Fighter
 'use strict';
 
-// let user = { name: 'Ivan' };
+// // let user = { name: 'Ivan' };
 
+
+// // let map = new WeakMap();
+// // map.set(user, 'data');
+// // // const arr = {user};
+// // user = null;
+
+// // // console.log(user);
+// // // console.log(arr[0]);
+// // console.log(map.has(user));
+// // console.log(map);
+
+
+// let cash = new WeakMap();
+
+// function cashUser(user) {
+//     if (!cash.has(user)) {
+//         cash.set(user, 'Date.now')
+//     }
+//     return cash.get(user);
+// }
+
+// let lena = { name: 'Elena' };
+// let alex = { name: 'Alex' };
+
+// cashUser(lena);
+// cashUser(alex);
+
+// lena = null;
+
+// console.log(cash.has(lena));
+// console.log(cash.has(alex));
+
+// //Weak Set
+// // add , has, delete 
+
+// let messages = [
+//     { text: 'Hello ', from: 'John' },
+//     { text: 'World ', from: 'Alex' },
+//     { text: '...... ', from: 'M' },
+// ];
+
+// let readMessages = new WeakSet();
+
+// readMessages.add(messages[0]);
+// // readMessages.add(messages[1]);
+
+// readMessages.add(messages[0]);
+
+// messages.shift();
+
+// console.log(readMessages.has(messages[0]));
+
+
+
+// let user = {name: 'Ivan'};
 
 // let map = new WeakMap();
-// map.set(user, 'data');
-// // const arr = {user};
+// map.set(user,'data');   
+
+// // const arr = [user];
 // user = null;
 
-// // console.log(user);
-// // console.log(arr[0]);
-// console.log(map.has(user));
 // console.log(map);
+// console.log(map.has(user));
 
 
-let cash = new WeakMap();
+// console.log(user);
+// console.log(arr[0]);
 
-function cashUser(user) {
-    if (!cash.has(user)) {
-        cash.set(user, 'Date.now')
-    }
-    return cash.get(user);
-}
+// let cache = new WeakMap();
 
-let lena = { name: 'Elena' };
-let alex = { name: 'Alex' };
+// function cacheUser(user) {
+//     if (cache.has(user)){
+//         cache.set(user, Date.now()); 
+//     }
 
-cashUser(lena);
-cashUser(alex);
+//     return cache.get(user); 
+// }
 
-lena = null;
 
-console.log(cash.has(lena));
-console.log(cash.has(alex));
 
-//Weak Set
-// add , has, delete 
+// let lena = {name: 'Elena'};
+// let alex = {name: 'Alex'};
+// cacheUser(lena);
+// cacheUser(alex);
+
+// lena = null;
+// console.log(cache.has(lena));
+// console.log(cache.has(alex)); 
+
+
+// let messages = [
+//     {text: 'Hello ', from: 'John'},
+//     {text: 'World ', from: 'Alex'},
+//     {text: '...... ', from: 'M'},
+// ];
+
+// let readMessages = new WeakSet();
+
+// readMessages.add(messages[0]);
+// readMessages.add(messages[1]);
 
 let messages = [
-    { text: 'Hello ', from: 'John' },
-    { text: 'World ', from: 'Alex' },
-    { text: '...... ', from: 'M' },
+    {text: 'Hello ', from: 'John'},
+    {text: 'World ', from: 'Alex'},
+    {text: '...... ', from: 'M'},
 ];
 
 let readMessages = new WeakSet();
 
 readMessages.add(messages[0]);
-// readMessages.add(messages[1]);
+readMessages.add(messages[1]);
 
 readMessages.add(messages[0]);
 
 messages.shift();
 
-console.log(readMessages.has(messages[0]));
-
-
+readMessages.add(readMessages.has(messages[0]));
