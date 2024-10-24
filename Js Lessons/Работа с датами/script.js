76f0284a7e811e8b483e4d2d060fe3a39689fb15
Hunter <xujanov.xujamurod@gmail.com>
2024-10-24 09:25:13 +0500

    работа с датами

---
+++ Js Lessons/Работа с датами/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const now = new Date('2024-10-25');
+// new Date.parse('2024-10-25');
+
+
+console.log(now.setHours(40));
+console.log(now);
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUtUTCHours());
+
+
+// console.log(now.getTimezoneOffset());
+
+
+// console.log(now.getTime());
+
+
+let start = new Date();
+
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд `);
