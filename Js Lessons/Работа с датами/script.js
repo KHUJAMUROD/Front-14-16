10750c865bbbcc2a69bae7b37b9f0a4d305e883e
Hunter <xujanov.xujamurod@gmail.com>
2024-10-29 09:25:52 +0500

    параметры окна

--- Js Lessons/Работа с датами/script.js
+++ Js Lessons/Работа с датами/script.js
@@ -1,36 +1,78 @@
+// const now = new Date('2024-10-25');
+
+// // new Date.parse('2024-10-25');
+
+
+// console.log(now.setHours(40));
+// console.log(now);
+
+
+// // console.log(now.getFullYear());
+// // console.log(now.getMonth());
+// // console.log(now.getDate());
+// // console.log(now.getDay());
+// // console.log(now.getHours());
+// // console.log(now.getUtUTCHours());
+
+
+// // console.log(now.getTimezoneOffset());
+
+
+// // console.log(now.getTime());
+
+
+// let start = new Date();
+
+
+// for (let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// }
+
+
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end - start} миллисекунд `);
+
+
+// const now = new Date('2020-05-01');
 'use strict';
 
 
-const now = new Date('2024-10-25');
-// new Date.parse('2024-10-25');
 
+// const now = new Date(2024, 5, 1, 24);
+const now = new Date();
 
-console.log(now.setHours(40));
+console.log(now.setHours(18));
+
 console.log(now);
 
 
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getDay());
-// console.log(now.getHours());
-// console.log(now.getUtUTCHours());
 
 
-// console.log(now.getTimezoneOffset());
+// console.log(now);
+// console.log(now. getFullYear());
+// console.log(now. getMonth());
+// console.log(now. getDate());
+// console.log(now. getDay());
+// console.log(now. getHours());
+// console.log(now. getUTCDate());
 
+
+// console.log(now.getTimezoneOffset() / -60);л
 
 // console.log(now.getTime());
 
 
-let start = new Date();
+// let start = new Data()
 
 
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3;
-}
+// for (let i = 0; i < 1000; i++) {
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end - start} миллисекунд `);
 
 
-let end = new Date();
 
-alert(`Цикл отработал за ${end - start} миллисекунд `);
