7e16cd12bff8073a43ca6a86f6b1431baad72d8b
HUNTER <xujanov.xujamurod@gmail.com>
2024-11-05 09:22:23 +0500

    2 new lessons aded

--- Js Lessons/food-dist/js/script.js
+++ Js Lessons/food-dist/js/script.js
@@ -164,17 +164,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Timer
 
+    const deadline = '2025-06-11';
 
-    const dedline = '2025-05-11';
-
-    function getTimeremaining(endtime) {
-
-        const t = new Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            seconds = Math.floor((t / 1000) % 60),
             minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
-
+            hours = Math.floor((t / (1000 * 60 * 60) % 24));
 
         return {
             'total': t,
@@ -183,48 +180,42 @@ window.addEventListener('DOMContentLoaded', function () {
             'minutes': minutes,
             'seconds': seconds
         };
-
     }
 
     function getZero(num) {
         if (num >= 0 && num < 10) {
-            return `0${num}`;
+            return '0' + num;
         } else {
             return num;
         }
     }
 
-
     function setClock(selector, endtime) {
-        const timer = document.querySelector(selector);
-        days = timer.querySelector("#days"),
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
-
         updateClock();
 
-
         function updateClock() {
-            const t = getTimeremaining(endtime);
+            const t = getTimeRemaining(endtime);
 
-            function updateClock() {
-                const t = getTimeremaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
-                days.innerHTML = getZero(t.days);
-                hours.innerHTML = getZero(t.hours);
-                minutes.innerHTML = getZero(t.minutes);
-                seconds.innerHTML = getZero(t.seconds);
-
-                if (t.total <= 0) {
-                    clearInterval(timeInterval);
-                }
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
             }
         }
-        setClock('.timer', dedline);
-    };
+    }
+
+    setClock('.timer', deadline);
 
 
 
@@ -277,3 +268,8 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     window.addEventListener('scroll', showModalByScroll);
 });
+
+
+
+
+
---
+++ Js Lessons/function-constraction/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// const num = new Number(3);
+// const num = new Function(3);
+// console.log(num);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+
+    };
+}
+
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log(`Hello${this.name}`)
+    }
+    exit() {
+        console.log(`Пользовотель ${this.name} ушел`)
+
+    }
+}
+
+
+User.prototype.exit = function (name) {
+    console.log(`Пользователь ${this.name} ушел`);
+
+};
+
+const baxa = new User('Baxa', 15);
+const alex = new User('Alex', 20);
+
+baxa.exit();
+
+baxa.hello();
+alex.hello();
+
+console.log(baxa);
+console.log(alex);
+
+
